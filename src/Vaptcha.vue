b4f24bc94432a6579ad96e2084ef4e87f27fc4ea
caesarleong <11495688+caesarleong@users.noreply.github.com>
2021-12-27 10:55:16 +0800

    Update Vaptcha.vue
    use svg to replace gif

--- src/Vaptcha.vue
+++ src/Vaptcha.vue
@@ -2,10 +2,31 @@
   <div ref="vaptcha">
     <div class="vaptcha-init-main">
       <div class="vaptcha-init-loading">
-        <a href="https://www.vaptcha.com/" target="_blank">
-          <img src="https://cdn.vaptcha.com/vaptcha-loading.gif">
+        <a href="/" target="_blank">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            width="48px"
+            height="60px"
+            viewBox="0 0 24 30"
+            style="enable-background: new 0 0 50 50; width: 14px; height: 14px; vertical-align: middle"
+            xml:space="preserve"
+          >
+            <rect x="0" y="9.22656" width="4" height="12.5469" fill="#CCCCCC">
+              <animate attributeName="height" attributeType="XML" values="5;21;5" begin="0s" dur="0.6s" repeatCount="indefinite"></animate>
+              <animate attributeName="y" attributeType="XML" values="13; 5; 13" begin="0s" dur="0.6s" repeatCount="indefinite"></animate>
+            </rect>
+            <rect x="10" y="5.22656" width="4" height="20.5469" fill="#CCCCCC">
+              <animate attributeName="height" attributeType="XML" values="5;21;5" begin="0.15s" dur="0.6s" repeatCount="indefinite"></animate>
+              <animate attributeName="y" attributeType="XML" values="13; 5; 13" begin="0.15s" dur="0.6s" repeatCount="indefinite"></animate>
+            </rect>
+            <rect x="20" y="8.77344" width="4" height="13.4531" fill="#CCCCCC">
+              <animate attributeName="height" attributeType="XML" values="5;21;5" begin="0.3s" dur="0.6s" repeatCount="indefinite"></animate>
+              <animate attributeName="y" attributeType="XML" values="13; 5; 13" begin="0.3s" dur="0.6s" repeatCount="indefinite"></animate>
+            </rect>
+          </svg>
         </a>
-        <span class="vaptcha-text">VAPTCHA启动中...</span>
+        <span class="vaptcha-text">Vaptcha启动中...</span>
       </div>
     </div>
   </div>
